aa54358f412c9ba77d85d62533b2fb00558787c7
Dmytro BORYSENKO <dborysen@e1r8p7.unit.ua>
2018-05-11 11:11:29 +0300

    11_05 1

--- assembler/main.c
+++ assembler/main.c
@@ -14,20 +14,24 @@
 
 void	show_usage(char *str)
 {
-	ft_printf("\n\e[1;37mUSAGE:	%s <sourcefile.s>\e[0m\e[0m\n\n", str);
+	ft_printf("\n\e[1;37mUSAGE:	%s <sourcefile.s>\e[0m\n\n", str);
 }
 
 int		main(int argc, char **argv)
 {
 	t_data	*data_from_file;
+	int		i;
 
+	i = 1;
 	data_from_file = NULL;
-	if (argc >= 2)
-	{
-		read_data(&data_from_file, argv[argc - 1]);
-		validation(data_from_file, argv[argc - 1]);
-		free_list(&data_from_file);
-	}
+	if (argc > 1)
+		while (i < argc)
+		{
+			read_data(&data_from_file, argv[i]);
+			validation(data_from_file, argv[i]);
+			free_list(&data_from_file);
+			i++;
+		}
 	else
 		show_usage(argv[argc - 1]);
 	return (0);
